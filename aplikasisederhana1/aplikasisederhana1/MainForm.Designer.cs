fd37ff6f0811343cb43c41380ba76744440698a6
Thaufan00 <77445263+Thaufan00@users.noreply.github.com>
2022-03-01 02:14:56 +0700

    Add files via upload

---
+++ aplikasisederhana1/aplikasisederhana1/MainForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: TUF
+ * Date: 3/1/2022
+ * Time: 1:15 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace aplikasisederhana1
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button button1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(382, 33);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(184, 56);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Pengisian Biodata";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(147, 186);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(225, 22);
+			this.textBox1.TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(147, 250);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(225, 22);
+			this.textBox2.TabIndex = 2;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(147, 326);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(225, 24);
+			this.comboBox1.TabIndex = 3;
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 16;
+			this.listBox1.Location = new System.Drawing.Point(488, 135);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(474, 372);
+			this.listBox1.TabIndex = 5;
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(25, 185);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "Nama";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(25, 249);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 23);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "NIM";
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(25, 329);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(100, 23);
+			this.label4.TabIndex = 8;
+			this.label4.Text = "KOM";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(196, 421);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(100, 23);
+			this.button1.TabIndex = 9;
+			this.button1.Text = "Tampilkan";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(974, 530);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "Nama";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
