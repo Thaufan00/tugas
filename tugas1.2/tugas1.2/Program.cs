b46ee5d5d560a720036868ccc9206cb9c35a6bdf
Thaufan00 <77445263+Thaufan00@users.noreply.github.com>
2022-03-01 02:16:38 +0700

    Add files via upload

---
+++ tugas1.2/tugas1.2/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: TUF
+ * Date: 2/26/2022
+ * Time: 3:44 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace tugas1._
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Write("Nama kamu siapa?  ");
+			
+			string nama = Console.ReadLine();
+			Console.WriteLine("Nama saya "+nama);
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
