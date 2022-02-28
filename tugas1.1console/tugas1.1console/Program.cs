95a7f4d1655f55459b2c5cb380319f83c84d6478
Thaufan00 <77445263+Thaufan00@users.noreply.github.com>
2022-03-01 02:16:06 +0700

    Add files via upload

---
+++ tugas1.1console/tugas1.1console/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: TUF
+ * Date: 2/26/2022
+ * Time: 3:43 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace tugas1._console
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
